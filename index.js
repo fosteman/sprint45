7860662033eb6826be00c270becdc73751da785f
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-11-08 12:18:14 -0500

    gh-pages cleanup

--- index.js
+++
@@ -1,8 +0,0 @@
-import {AppRegistry} from 'react-360'
-import LocationHeader from './LocationHeader'
-import LocationBackground from './LocationBackground'
-import LocationDetails from './LocationDetails'
-
-AppRegistry.registerComponent('LocationHeader', () => LocationHeader)
-AppRegistry.registerComponent('LocationBackground', () => LocationBackground)
-AppRegistry.registerComponent('LocationDetails', () => LocationDetails)
